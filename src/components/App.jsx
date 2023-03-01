993248bf4c587a20e76ec65c509a6419e2916f14
Mykola <107625914+kolya-kyb@users.noreply.github.com>
2023-03-01 18:50:42 +0200

    add redux

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from 'react-redux';
+
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import ContactForm from './ContactForm/ContactForm';
+
+import { Wrapper } from './App.module';
+
+export const App = () => {
+  return (
+    <Wrapper>
+      <h2>Phonebook</h2>
+      <ContactForm />
+      <div>
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
+    </Wrapper>
+  );
+};
